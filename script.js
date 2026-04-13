3f5dcb9de29e124e6e3d3e82711cfab55b1a4945
aniqahnaveed09-web <aniqahnaveed09@gmail.com>
2026-04-13 22:48:15 +0500

    my work

---
+++ script.js
@@ -0,0 +1,98 @@
+// question 1 
+
+
+// var Name = ["Aniqah", "Naveed"]
+// var result = Name.join()
+
+// alert("Thanks ," + result)
+
+// question 2
+
+// var mobile = prompt("enter your favorite mobile model")
+// mobile.length
+// document.write("my favorite mobile model is:" + mobile + "<br>")
+// document.write("the lenght is : " + mobile.length)
+
+// question 3
+// var nationality = "Pakistani"
+//  nationality.indexOf
+// document.write("string:" + nationality + "<br>")
+// document.write("Index of 'n' :" + nationality.indexOf("n") )
+
+
+// question 4
+// var greet = "hello world"
+// var lastIndex = greet.lastIndexOf("l");
+// document.write("string :" + greet + "<br>")
+// document.write( "the last index of l is :" + lastIndex)
+
+// question 5
+// var nationality = "Pakistani"
+// var character = nationality.charAt(3)
+// document.write("string:" + nationality + "<br>")
+// document.write("character at index 3 is:" + character )
+
+// question 6
+
+
+// const firstName = "aniqah";
+// const lastName = "naveed";
+
+// const fullName = firstName.concat(" ", lastName);
+
+// alert( "thanks!" + fullName);
+
+// question 7
+
+// var city = "Hyderabad"
+// var replace = city.replace("Hyder", "Islam")
+// document.write("the city is:" + city + "<br>")
+// document.write("replace by:" + replace )
+
+// question 8
+
+
+// var sentence = "Ali and Sami are bestfriends. they play cricket and football together"
+// var replace = sentence.replaceAll("and" , "&")
+// document.write("original sentence:" + sentence + "<br>")
+// document.write("after replace:" + replace)
+
+
+// question 9
+
+
+// var strValue = "472";
+
+// var numValue = Number(strValue);
+// document.write("Value: " + strValue + "<br>");
+// document.write("Type: " + typeof strValue + "<br>");
+
+// document.write("Value: " + numValue + "<br>");
+// document.write("Type: " + typeof numValue + "<br>");
+
+
+// question 10 
+
+// var dryfruit = prompt("enter your favorite dry fruit")
+// var fruit = dryfruit.toUpperCase()
+// document.write("the input is :" + dryfruit +"<br>")
+// document.write("to upper case :" + fruit +"<br>")
+
+// question 11
+// var userInput = prompt("Enter a word:");
+// var firstChar = userInput.slice(0, 1).toUpperCase();
+// var otherChars = userInput.slice(1).toLowerCase();
+// var titleCase = firstChar + otherChars;
+// document.write("User input: " + userInput + "<br>");
+// document.write("Title case: " + titleCase);
+
+// question 12
+
+var num = 35.36;
+var numAsString = num.toString();
+
+var result = numAsString.replace(".", "");
+document.write("Number: " + num + "<br>");
+document.write("Result: " + result);
+
+
